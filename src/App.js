49c20f98b4c9ad4eac41231e8d2b9404f9413185
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-03-05 13:20:38 -0500

    add absolute artist image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import './App.scss';
+
+import artistImage from './assets/images/The-Weeknd-PNG-Images.png';
+
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <img src={artistImage} alt="" />
     </div>
   );
 }
